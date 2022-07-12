de6c1cd0f3c3ea0dd7bdf595bbb3a45aba6ac542
Leda <ledathemis@protonmail.com>
2022-07-12 13:19:21 +0000

    add category controller

---
+++ controllers/categoryController.js
@@ -0,0 +1,27 @@
+exports.category_detail = function (req, res, next) {
+  res.send('NOT IMPLEMENTED');
+};
+
+exports.category_create_get = function (req, res, next) {
+  res.send('NOT IMPLEMENTED');
+};
+
+exports.category_create_post = function (req, res, next) {
+  res.send('NOT IMPLEMENTED');
+};
+
+exports.category_delete_get = function (req, res, next) {
+  res.send('NOT IMPLEMENTED');
+};
+
+exports.category_delete_post = function (req, res, next) {
+  res.send('NOT IMPLEMENTED');
+};
+
+exports.category_update_get = function (req, res, next) {
+  res.send('NOT IMPLEMENTED');
+};
+
+exports.category_update_post = function (req, res, next) {
+  res.send('NOT IMPLEMENTED');
+};
