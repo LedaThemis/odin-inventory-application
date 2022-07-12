6c2f22c4fb0eeed102dec030e48c61c4e895db1d
Leda <ledathemis@protonmail.com>
2022-07-12 13:12:03 +0000

    add category routes

--- routes/index.js
+++ routes/index.js
@@ -31,4 +31,27 @@ router.post('/item/:id/update', item_controller.item_update_get);
 // GET request for one item
 router.get('/item/:id', item_controller.item_detail);
 
+// CATEGORY ROUTES
+
+// GET request to create Category
+router.get('/category/create', category_controller.category_create_get);
+
+// POST request to create Category
+router.post('/category/create', category_controller.category_create_post);
+
+// GET request to delete Category
+router.get('/category/:id/delete', category_controller.category_delete_get);
+
+// POST request to delete Category
+router.post('/category/:id/delete', category_controller.category_delete_post);
+
+// GET request to update Category
+router.get('/category/:id/update', category_controller.category_update_get);
+
+// POST request to update Category
+router.post('/category/:id/update', category_controller.category_update_get);
+
+// GET request for one Category
+router.get('/category/:id', category_controller.category_detail);
+
 module.exports = router;
