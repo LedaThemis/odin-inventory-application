3f3854da28ed88660f2df1e4b68688aa3fd56931
Leda <ledathemis@protonmail.com>
2022-07-12 13:19:07 +0000

    add item controller

---
+++ controllers/itemController.js
@@ -0,0 +1,30 @@
+exports.item_detail = function (req, res, next) {
+    res.send('NOT IMPLEMENTED');
+  };
+  
+  exports.item_create_get = function (req, res, next) {
+    res.send('NOT IMPLEMENTED');
+  };
+  
+  exports.item_create_post = function (req, res, next) {
+    res.send('NOT IMPLEMENTED');
+  };
+  
+  exports.item_delete_get = function (req, res, next) {
+    res.send('NOT IMPLEMENTED');
+  };
+  
+  exports.item_delete_post = function (req, res, next) {
+    res.send('NOT IMPLEMENTED');
+  };
+  
+  exports.item_update_get = function (req, res, next) {
+    res.send('NOT IMPLEMENTED');
+  };
+  
+  exports.item_update_post = function (req, res, next) {
+    res.send('NOT IMPLEMENTED');
+  };
+  
+  
+  
