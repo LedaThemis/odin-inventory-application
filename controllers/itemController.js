3f80dd63c0703dda3197a43c50c0f223aa9ab852
Leda <ledathemis@protonmail.com>
2022-07-12 13:53:21 +0000

    chore: format

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,30 +1,27 @@
 exports.item_detail = function (req, res, next) {
-    res.send('NOT IMPLEMENTED');
-  };
-  
-  exports.item_create_get = function (req, res, next) {
-    res.send('NOT IMPLEMENTED');
-  };
-  
-  exports.item_create_post = function (req, res, next) {
-    res.send('NOT IMPLEMENTED');
-  };
-  
-  exports.item_delete_get = function (req, res, next) {
-    res.send('NOT IMPLEMENTED');
-  };
-  
-  exports.item_delete_post = function (req, res, next) {
-    res.send('NOT IMPLEMENTED');
-  };
-  
-  exports.item_update_get = function (req, res, next) {
-    res.send('NOT IMPLEMENTED');
-  };
-  
-  exports.item_update_post = function (req, res, next) {
-    res.send('NOT IMPLEMENTED');
-  };
-  
-  
-  
+  res.render('NOT IMPLEMENTED');
+};
+
+exports.item_create_get = function (req, res, next) {
+  res.send('NOT IMPLEMENTED');
+};
+
+exports.item_create_post = function (req, res, next) {
+  res.send('NOT IMPLEMENTED');
+};
+
+exports.item_delete_get = function (req, res, next) {
+  res.send('NOT IMPLEMENTED');
+};
+
+exports.item_delete_post = function (req, res, next) {
+  res.send('NOT IMPLEMENTED');
+};
+
+exports.item_update_get = function (req, res, next) {
+  res.send('NOT IMPLEMENTED');
+};
+
+exports.item_update_post = function (req, res, next) {
+  res.send('NOT IMPLEMENTED');
+};
